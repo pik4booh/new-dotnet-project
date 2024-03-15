7ed6756c6d8f79b3e324a95b526159f7c2752426
pik4booh <prncrobins2003@gmail.com>
2024-03-16 01:28:54 +0300

    first commit

---
+++ Data/apiContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data;
+
+public partial class apiContext : DbContext
+{
+    public apiContext()
+    {
+    }
+
+    public apiContext(DbContextOptions<apiContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+		if (!optionsBuilder.IsConfigured)
+		{
+            optionsBuilder.UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=hehe;");
+            optionsBuilder.UseLazyLoadingProxies();
+		}
+
+	}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
